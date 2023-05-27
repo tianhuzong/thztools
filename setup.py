e402ee113bf28262c333a4c6af1a80c3c4bece7e
sen <tianhuzong@qq.com>
2023-05-27 15:15:11 +0800

    修复1.0.4中的命名冲突

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("./README.md",mode='r',encoding='utf-8') as f:
     des = f.read()
 setup(
     name="thztools",      # 包名，用于安装和调用该包
-    version="1.0.4",               # 版本号
+    version="1.0.5",               # 版本号
     author="Sen",
     description="由天狐宗开发的工具，方便开发时使用",
     long_description=des,
--- thztools/jiami.py
+++ thztools/jiami.py
@@ -1,4 +1,4 @@
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES as AESclass
 from Crypto.Random import get_random_bytes
 import rsa
 class KaiSa:
@@ -108,7 +108,7 @@ class AES:
             密文,随机数，验证标签，均为bytes类型
         """
         # 用秘钥创建一个AES对象
-        cipher = AES.new(key, AES.MODE_EAX)
+        cipher = AESclass.new(key, AESclass.MODE_EAX)
         # 加密明文
         ciphertext, tag = cipher.encrypt_and_digest(plaintext.encode('utf-8'))
         # 返回加密后的密文和必要的参数
@@ -123,7 +123,7 @@ class AES:
         tag: 加密时生成的验证标签
         """
         # 用密钥和随机数创建一个AES对象
-        cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+        cipher = AESclass.new(key, AESclass.MODE_EAX, nonce=nonce)
         try:
             # 解密密文
             plaintext = cipher.decrypt_and_verify(ciphertext, tag)
