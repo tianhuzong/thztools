4276b3aaf31f4ab7e39b2b3a3bedc6509caabb97
thzsen <tianhuzong@qq.com>
2023-06-24 13:40:54 +0800

    更新了版本，将RSA密钥格式修改为PEM，RSA，AES的密文和明文修改为base64，RSA的签名文本，AES秘钥也修改为base64

--- build/lib/tianhutools/jiami.py
+++ build/lib/tianhutools/jiami.py
@@ -1,6 +1,12 @@
 from Crypto.Cipher import AES as AESclass
 from Crypto.Random import get_random_bytes
 import rsa
+import base64
+
+def __bytes_to_b64(bytes_var : bytes) -> str:
+    return base64.b64decode(bytes_var)
+def __b64_to_bytes(b64str : str) -> bytes:
+    return base64.b64encode(b64str)
 class KaiSa:
     def jiami(self,plaintext, shift):
         """
@@ -46,47 +52,51 @@ class RSA:
         :return 公钥,私钥
         """
         (gongyao,siyao) = rsa.newkeys(length)
-        return gongyao,siyao
-    def jiami(self,gongyao,text) -> bytes:
+        gongyao_save = gongyao.save_pkcs1().decode()
+        siyao_save = siyao.save_pkcs1().decode()
+        return gongyao_save,siyao_save
+    def jiami(self,gongyao,text) -> str:
         """
         使用rsa算法进行加密
         Args:
-            gongyao : rsa 公钥
+            gongyao : rsa 公钥pem格式
             text : 欲加密文本
         Return:
-            返回密文(bytes流)
+            返回密文(base64)
         """
         message = text.encode()
-        miwen = rsa.encrypt(message, gongyao)
+        gongyao_obj = rsa.PublicKey.load_pkcs1(gongyao.encode())
+        miwen = base64.b64decode( rsa.encrypt(message, gongyao_obj) ).decode()
         return miwen
-    def jiemi(self,siyao,miwen:bytes):
+    def jiemi(self,siyao,miwen:str):
         """
         使用rsa算法进行解密
         Args:
-            siyao : rsa私钥
-            miwen : 密文（bytes字节流）
+            siyao : rsa私钥,pem格式
+            miwen : 密文（base64）
         Return:
             明文
         """
-        return rsa.decrypt(miwen, siyao).decode()
+        return rsa.decrypt(base64.b64encode(miwen), siyao).decode()
     def sign(self,siyao,text):
         """
         使用rsa算法进行数字签名
-        :param siyao:rsa私钥
+        :param siyao:rsa私钥,pem格式
         :param text:欲签名文本
         :return 返回签名文本
         """
-        qianminwenben = rsa.sign(text.encode(),siyao,'SHA-1')
+        siyao_obj = rsa.PrivateKey.load_pkcs1(siyao)
+        qianminwenben = base64.b64decode( rsa.sign(text.encode(),siyao_obj,'SHA-1') ).decode()
         return qianminwenben
     def qmyz(self,text,qmwb,gongyao):
         """
         签名验证
         :param text:被验证的文本
         :param qmwb:签名文本，sign()函数的返回值
-        :param gongyao:rsa公钥
+        :param gongyao:rsa公钥,pem格式
         :return 成功返回True，失败返回False
         """
-        res = rsa.verify(text.encode(),qmwb,gongyao)
+        res = rsa.verify(text.encode(),base64.b64encode(qmwb),rsa.PublicKey.load_pkcs1(gongyao))
         if res:
             return True
         else:
@@ -95,24 +105,25 @@ class AES:
     def newkey(self):
         """
         自动生成32位（256位）AES秘钥
+        :return key，base64编码的key
         """
         key = get_random_bytes(32)
-        return key
+        return base64.b64decode(key).decode()
 
     def jiami(self,plaintext, key):
         """
         AES加密算法
         plaintext: 明文
-        key: 秘钥
+        key: 秘钥,base64
         Returns:
-            密文,随机数，验证标签，均为bytes类型
+            密文,随机数，验证标签，均为str类型的base64编码
         """
         # 用秘钥创建一个AES对象
-        cipher = AESclass.new(key, AESclass.MODE_EAX)
+        cipher = AESclass.new(base64.b64encode(key), AESclass.MODE_EAX)
         # 加密明文
         ciphertext, tag = cipher.encrypt_and_digest(plaintext.encode('utf-8'))
         # 返回加密后的密文和必要的参数
-        return ciphertext, cipher.nonce, tag
+        return __bytes_to_b64(ciphertext), __bytes_to_b64(cipher.nonce), __bytes_to_b64(tag)
 
     def jiemi(self,ciphertext, key, nonce, tag):
         """
@@ -123,10 +134,10 @@ class AES:
         tag: 加密时生成的验证标签
         """
         # 用密钥和随机数创建一个AES对象
-        cipher = AESclass.new(key, AESclass.MODE_EAX, nonce=nonce)
+        cipher = AESclass.new(__b64_to_bytes(key), AESclass.MODE_EAX, nonce=__b64_to_bytes(nonce))
         try:
             # 解密密文
-            plaintext = cipher.decrypt_and_verify(ciphertext, tag)
+            plaintext = cipher.decrypt_and_verify(__b64_to_bytes(ciphertext), __b64_to_bytes(tag))
             return plaintext.decode('utf-8')
         except:
             return None
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("./README.md",mode='r',encoding='utf-8') as f:
     des = f.read()
 setup(
     name="tianhutools",      # 包名，用于安装和调用该包
-    version="1.0.1",               # 版本号
+    version="1.0.2",               # 版本号
     author="Sen",
     description="由天狐宗开发的工具，方便开发时使用,已更名为tianhutools",
     long_description=des,
@@ -17,7 +17,8 @@ setup(
         "rsa",
         "Crypto",
         "2ip",
-        "sumy"
+        "sumy",
+        "jieba"
     ],
     classifiers=[                # 分类标签（可选），使用 PyPI 标准分类
         "Programming Language :: Python :: 3",
